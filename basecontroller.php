f8ded04e6f89885d424d38314a303a5d31ede607
Stuart Dallas <github@3ft9.com>
2012-01-21 18:49:09 +0000

    Added redirect and modifyURL methods from other projects to the basecontroller.

--- basecontroller.php
+++ basecontroller.php
@@ -27,4 +27,83 @@
 		{
 			$this->_view = View::create(self::$_template_root);
 		}
+
+		/**
+		 * Redirect the browser to a different location.
+		 *
+		 * @param string $url The destination URL - according to the spec this
+		 *                    should be absolute not relative.
+		 * @param bool $exit Set to false to return to the caller rather than
+		 *                   ending execution.
+		 */
+		public function redirect($url, $exit = true, $permanent = false)
+		{
+			if ((substr($url, 0, 7) != 'http://' || substr($url, 0, 8) != 'https://') and $url[0] == '/') {
+				$url = 'http://'.$_SERVER['HTTP_HOST'].$url;
+			}
+
+			if (headers_sent()) {
+				echo '<script type="text/javascript"><!--'.PHP_EOL.'location.href = \''.$url.'\';'.PHP_EOL.'--></script>'.PHP_EOL;
+			} else {
+				// Output the redirect header
+				header('Location: '.$url, true, ($permanent ? 301 : 302));
+			}
+
+			// If told to exit, output the moved message and do so
+			if ($exit) {
+				// Empty and clean up any pending output buffers
+				while (@ob_end_clean());
+
+				print '<h1>Document Moved</h1>';
+				print '<p>The requested document has moved <a href="'.$url.'">here</a>.</p>';
+				print '<script type="text/javascript"> location.href = "'.$url.'"; </script>';
+				exit;
+			}
+		}
+
+		/**
+		 * Simple function to modify a URL by adding or overriding GET vars.
+		 *
+		 * @param string $url The URL to be modified, or false to use the request URL.
+		 * @param array $addvars The querystring variables to be added.
+		 * @param array $removevars The querystring variables to be removed.
+		 * @param array $preservedvars Will not be changed if they already exist.
+		 * @return string The new URL.
+		 */
+		public function modifyURL($url = false, $addvars = array(), $removevars = array(), $preservedvars = array())
+		{
+			if ($url === false) {
+				$url = $_SERVER['REQUEST_URI'];
+			}
+
+			// Are we making any changes?
+			if (count($addvars) == 0 and count($removevars) == 0) {
+				return $url;
+			}
+
+			// Do we have an existing query string?
+			if (strpos($url, '?') === false) {
+				// Nope, so just add the ones that need adding
+				$parsed_params = $addvars;
+			} else {
+				// Grab the current vars
+				list($url, $params) = explode('?', $url, 2);
+				parse_str($params, $parsed_params);
+				// Remove those that need to be removed
+				foreach ($removevars as $key) {
+					if (!in_array($key, $preservedvars)) {
+						unset($parsed_params[$key]);
+					}
+				}
+				// Add those that need to be added
+				foreach ($addvars as $key => $val) {
+					if (!in_array($key, $preservedvars)) {
+						$parsed_params[$key] = $val;
+					}
+				}
+			}
+
+			// Return the modified URL
+			return $url.(empty($parsed_params) ? '' : '?'.http_build_query($parsed_params));
+		}
 	}
