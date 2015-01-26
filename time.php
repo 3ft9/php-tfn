c72a8337ad0791adbc6c419f39a90946569f20b3
Stuart Dallas <github@3ft9.com>
2015-01-26 22:48:52 +0000

    Finally finished the Time::toRelative method.

--- time.php
+++ time.php
@@ -34,23 +34,38 @@
 
 		public static function toRelative($time, $now = false)
 		{
-			if ($now === false) {
-				$now = time();
+			$diff = ($now ? $now : time()) - $time;
+			if($diff == 0) {
+				return 'now';
+			} elseif($diff > 0) {
+				$day_diff = floor($diff / 86400);
+				if($day_diff == 0) {
+					if($diff < 60) return 'just now';
+					if($diff < 120) return '1 minute ago';
+					if($diff < 3600) return floor($diff / 60) . ' minutes ago';
+					if($diff < 7200) return '1 hour ago';
+					if($diff < 86400) return floor($diff / 3600) . ' hours ago';
+				}
+				if($day_diff == 1) return 'Yesterday';
+				if($day_diff < 7) return $day_diff . ' days ago';
+				if($day_diff < 31) return ceil($day_diff / 7) . ' weeks ago';
+				if($day_diff < 60) return 'last month';
+				return date('F Y', $time);
+			} else {
+				$diff = abs($diff);
+				$day_diff = floor($diff / 86400);
+				if($day_diff == 0) {
+					if($diff < 120) return 'in a minute';
+					if($diff < 3600) return 'in ' . floor($diff / 60) . ' minutes';
+					if($diff < 7200) return 'in an hour';
+					if($diff < 86400) return 'in ' . floor($diff / 3600) . ' hours';
+				}
+				if($day_diff == 1) return 'Tomorrow';
+				if($day_diff < 4) return date('l', $time);
+				if($day_diff < 7 + (7 - date('w'))) return 'next week';
+				if(ceil($day_diff / 7) < 4) return 'in ' . ceil($day_diff / 7) . ' weeks';
+				if(date('n', $time) == date('n') + 1) return 'next month';
+				return date('F Y', $time);
 			}
-
-			$diff = $now - $time;
-
-			if ($diff < 0) {
-				return 'in the past';
-			}
-
-			if ($diff > (self::OneYear - self::ThirtyDays)) {
-				return 'about a year ago';
-			}
-
-			// TODO: Finish this!
-			// if ($diff > (self::SevenDays - self::OneDay)) {
-			// 	if ($diff > (self::))
-			// }
 		}
 	}
